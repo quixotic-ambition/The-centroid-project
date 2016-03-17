92711289eb967a50d528f14723308eb93c6811fb
Arun RK <goldenslice26@gmail.com>
2016-03-17 18:02:57 +0530

    general modification

--- centroid-app/centroid-app.js
+++ centroid-app/centroid-app.js
@@ -1,17 +1,35 @@
   if(Meteor.isClient){
 
-    var calculateClick=function(){
+    // var solution;
+    Template.mainPage.events({
+
+    "submit.mainForm":function(event){
+        
+        event.preventDefault();
+        var lat1=event.target['lat1'].value;
+        var lon1=event.target.lon1.value;
+        var lat2=event.target.lat2.value;
+        var lon2=event.target.lon2.value;
+        var lat2=event.target.lat3.value;
+        var lon2=event.target.lon3.value;
+        Session.set("solution",getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2));
+    },
+
+
+   });
+
+    Template.mainPage.helpers({
+
+      printSolution:function(){
+        console.log("enteres print func "+ Session.get("solution"));
+        if(Session.get("solution"))
+        return Session.get("solution");
+      },
       
-      console.log("entered calculate click");
-      var lat1=document.getElementById("lat1");
-      var lon1=document.getElementById("lon1");
-      var lat2=document.getElementById("lat2");
-      var lon2=document.getElementById("lon2");
-      console.log(lat1);
-    }
+    
+    });
 
-
-    var getDistanceFromLatLonInKm=function(lat1,lon1,lat2,lon2) {
+    function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
       var R = 6371; // Radius of the earth in km
       var dLat = deg2rad(lat2-lat1);  // deg2rad below
       var dLon = deg2rad(lon2-lon1); 
@@ -23,20 +41,12 @@
       var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
       var d = R * c; // Distance in km
       console.log(d);
-      console.log(lat1);
       return d;
     }
 
-  function deg2rad(deg) {
-    return deg * (Math.PI/180)
-  }
+    function deg2rad(deg) {
 
-
-    document.getElementById("calculate-butt").addEventListener("click",calculateClick);
-
-    var solution=getDistanceFromLatLonInKm;
-    console.log("distance should be : " + solution);
-
-    document.getElementById("solution").value=solution;
+            return deg * (Math.PI/180)
+          }
 
 }
