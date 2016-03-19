f9a893337e3c4f983eeee947d0c66674010c19ff
quixotic-ambition <arunrk1499@gmail.com>
2016-03-19 17:41:42 +0530

    change from fermat s to centroid

--- centroid-app/centroid-app.js
+++ centroid-app/centroid-app.js
@@ -1,19 +1,19 @@
   if(Meteor.isClient){
 
-    // var solution;
     Template.mainPage.events({
 
     "submit.mainForm":function(event){
         
+        console.log("Enter the main form func ");
         event.preventDefault();
         var lat1=event.target['lat1'].value;
         var lon1=event.target.lon1.value;
         var lat2=event.target.lat2.value;
         var lon2=event.target.lon2.value;
-        var lat2=event.target.lat3.value;
-        var lon2=event.target.lon3.value;
-        Session.set("solution",getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2));
-    },
+        var lat3=event.target.lat3.value;
+        var lon3=event.target.lon3.value;
+        Session.set("solution",getCentroid(lat1,lon1,lat2,lon2,lat3,lon3));
+    }, 
 
 
    });
@@ -29,24 +29,41 @@
     
     });
 
-    function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
-      var R = 6371; // Radius of the earth in km
-      var dLat = deg2rad(lat2-lat1);  // deg2rad below
-      var dLon = deg2rad(lon2-lon1); 
-      var a = 
-        Math.sin(dLat/2) * Math.sin(dLat/2) +
-        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-        Math.sin(dLon/2) * Math.sin(dLon/2)
-        ; 
-      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-      var d = R * c; // Distance in km
-      console.log(d);
-      return d;
+    function getCentroid(lat1,lon1,lat2,lon2,lat3,lon3){
+
+      console.log("enterred get centroid " + lat1);
+      var cLat;
+      var cLon;
+      console.log(((+lat1) + (+lat2) + (+lat3))/3);
+      cLat=((+lat1) + (+lat2) + (+lat3))/3;
+      cLon=((+lon1) + (+lon2) + (+lon3))/3;
+       console.log(cLat + " " + cLon);
+
+      return [cLat,cLon];
+
     }
 
-    function deg2rad(deg) {
+    //maybe for large diatances
+    // function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+    //   var R = 6371; // Radius of the earth in km
+    //   var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    //   var dLon = deg2rad(lon2-lon1); 
+    //   var a = 
+    //     Math.sin(dLat/2) * Math.sin(dLat/2) +
+    //     Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    //     Math.sin(dLon/2) * Math.sin(dLon/2)
+    //     ; 
+    //   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    //   var d = R * c; // Distance in km
+    //   console.log(d);
+    //   return d;
+    // }
 
-            return deg * (Math.PI/180)
-          }
+    // function deg2rad(deg) {
+
+    //         return deg * (Math.PI/180)
+    //       }
+
+
 
 }
